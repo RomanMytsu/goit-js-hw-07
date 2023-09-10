87cd4b668cd1a238c817f5d35dc0ccc545fad12a
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2023-09-10 14:17:48 +0300

    v2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+list.addEventListener("click", onClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    
+   <a class="gallery__link"${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+ </li>`
+    )
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const currentElement = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${currentElement}"/> `, {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscape);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscape);
+    },
+  });
+  instance.show();
+
+  function onEscape(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+  document.addEventListener("keydown", onEscape);
+}
